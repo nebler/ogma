8936cc9870f4bce9cd7efda44af956126b7beb28
nebler <benklernoahelias@gmail.com>
2023-01-04 20:25:38 +0100

    init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+
+import ("fmt", "os", "encoding/json")
+
+type Adress struct {
+	City string
+	State string 
+	Country string
+	Pincode json.Number
+}
+
+type User strcut {
+	Name string,
+	Age json.Number
+	Contact string
+	Company string
+}
+
+func main() {
+
+}
