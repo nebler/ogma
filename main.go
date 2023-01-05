54d03103ed67e0f0129b41f74e9b397ec050a672
nebler <benklernoahelias@gmail.com>
2023-01-05 23:41:38 +0100

    init employees

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
 
-import ("fmt", "os", "encoding/json")
-
-type Adress struct {
-	City string
-	State string 
+type Address struct {
+	City    string
+	State   string
 	Country string
 	Pincode json.Number
 }
 
-type User strcut {
-	Name string,
-	Age json.Number
+type User struct {
+	Name    string
+	Age     json.Number
 	Contact string
 	Company string
+	Address Address
+}
+
+type Database interface {
+}
+
+func New(dir string, huh string) {
+
 }
 
 func main() {
+	dir := "./"
 
+	db, err := New(dir, "")
+	if err != nil {
+		fmt.Print("error")
+	}
+
+	employees := []User{
+		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
+		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
+		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
+		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
+	}
 }
