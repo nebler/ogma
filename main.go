e4a9e350e99d5a1d33edb9d0eb302d1925e188fd
nebler <benklernoahelias@gmail.com>
2023-01-06 00:05:19 +0100

    reading all users

--- main.go
+++ main.go
@@ -36,9 +36,30 @@ func main() {
 	}
 
 	employees := []User{
-		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
-		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
-		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
-		{"John", "23", "2323434", "Myril Tech", Address{"test1", "test", "test", "41434434"}},
+		{"Noah", "25", "2323434", "dmTech", Address{"Karlsruhe", "BaWü", "Germany", "12345"}},
+		{"Paul", "25", "1234542", "Google", Address{"Berlin", "Berlin", "Germany", "45478"}},
+		{"Akhil", "25", "2323434", "Facebook", Address{"Karlsruhe", "BaWü", "Germany", "66666"}},
+		{"Antonia", "25", "2323434", "Sony", Address{"San Francisco", "Califronia", "Germany", "12345"}},
+		{"Marius", "25", "2323434", "Bosch", Address{"Karlsruhe", "BaWü", "Germany", "12345"}},
+	}
+
+	for _, value := range employees {
+		db.Write("users", value.Name, value)
+	}
+
+	records, err := db.ReadAll("users")
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	fmt.Println(records)
+
+	allusers := []User{}
+
+	for _, f := range records {
+		employeeFound := User{}
+		if err := json.Unmarshal([]byte(f), &employeeFound); err != nil {
+			fmt.Println("Error", err)
+		}
+		allusers = append(allusers, employeeFound)
 	}
 }
